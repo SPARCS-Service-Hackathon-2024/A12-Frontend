df29b58e392a7447766ed0aa4c6d4f43006081ef
JYBESiU <jychoi.besiu@gmail.com>
2024-02-15 20:42:05 +0900

    💄 main style 완성

---
+++ src/pageComponents/Main/Main.tsx
@@ -0,0 +1,87 @@
+import {
+  Box,
+  Center,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+
+import S from "./style.module.css";
+import { greenPointColor } from "@/constants/color";
+
+export interface MainProps {}
+
+function Main({}: MainProps) {
+  const router = useRouter();
+
+  return (
+    <Box
+      w={"100%"}
+      h={"100vh"}
+      backgroundSize={"contain"}
+      backgroundImage={"/images/main_background.png"}
+      position={"relative"}
+    >
+      <Box
+        left={"24px"}
+        top={"190px"}
+        position={"absolute"}
+      >
+        <Text
+          fontSize={"24px"}
+          fontWeight={300}
+          color={"rgba(255, 255, 255, 0.70)"}
+        >
+          먼지 쌓인 <span className={S.bold}>기억</span>을
+          <br />
+          <span className={S.bold}>기록</span>으로
+        </Text>
+      </Box>
+      <VStack
+        w={"100%"}
+        gap={"16px"}
+        bottom={"166px"}
+        position={"absolute"}
+      >
+        <Center
+          h={"56px"}
+          w={"300px"}
+          bg={"white"}
+          borderRadius={"10px"}
+          border={"1px solid #bababa"}
+        >
+          <Text
+            color={greenPointColor}
+            fontSize={"20px"}
+            fontWeight={600}
+          >
+            회원 가입
+          </Text>
+        </Center>
+        <Text
+          w={"100%"}
+          textAlign={"center"}
+          className={S.linedtext}
+        >
+          또는
+        </Text>
+        <Center
+          h={"56px"}
+          w={"300px"}
+          borderRadius={"10px"}
+          border={"1px solid #bababa"}
+        >
+          <Text
+            color={"white"}
+            fontSize={"20px"}
+            fontWeight={600}
+          >
+            로그인
+          </Text>
+        </Center>
+      </VStack>
+    </Box>
+  );
+}
+
+export default Main;
