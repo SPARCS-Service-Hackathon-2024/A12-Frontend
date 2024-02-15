83030b3bdd7f492b59afb9814140eade001090a7
JYBESiU <jychoi.besiu@gmail.com>
2024-02-15 14:21:12 +0900

    💄 bottom navigation style

--- next.config.js
+++ next.config.js
@@ -2,9 +2,15 @@ const withPWA = require("next-pwa")({
   dest: "public",
 });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = withPWA({
   reactStrictMode: true,
-};
-
-module.exports = withPWA(nextConfig);
+  webpack: (config) => {
+    // 아래를 추가합니다.
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+});
---
+++ src/svg/index.ts
@@ -0,0 +1,5 @@
+export { default as BookFillIcon } from "./book_fill.svg";
+export { default as BookOutlineIcon } from "./book_outline.svg";
+export { default as HomeFillIcon } from "./home_fill.svg";
+export { default as HomeOutlineIcon } from "./home_outline.svg";
+export { default as AddButtonIcon } from "./add_button.svg";
